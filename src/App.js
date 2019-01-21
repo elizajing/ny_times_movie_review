8a4772418c43bb4f8efc2610962ee4f3651e653a
Eliza Jing <elizajing@Elizas-MacBook-Pro-2.local>
2019-01-21 13:49:12 +0100

    Add conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import Reviews from './Reviews/reviews.js';
 import Search from './Search/search.js';
 
 class App extends Component {
+  constructor(props){
+    super(props);
 
+    this.handleReviewsClick = this.handleReviewsClick.bind(this);
+    this.handleSearchClick = this.handleSearchClick.bind(this);
+
+    this.state = {
+      reviews: false,
+      search: false,
+      home: true
+    }
+  }
+
+  handleReviewsClick(){
+    this.setState({
+      reviews: true,
+      search: false,
+      home: false
+    })
+  }
+
+  handleSearchClick(){
+    this.setState({
+      reviews: false,
+      search: true,
+      home: false
+    })
+  }
+  
   render() {
+    const {reviews, search} = this.state;
+    let div;
 
+    if(reviews){
+      div = <Reviews />
+    }else if(search){
+      div = <Search />
+    }else{
+      div = <p>Find the latest movie reviews from New York Times magazine!</p>
+    }
     return (
-      <div className="container">
-        <div className="reviews">
-          <h1>Latest Reviews</h1>
-          <Reviews />
+      <div>
+        <div className="header">
+          <div className="header-item">
+            <button className="button" onClick={this.handleReviewsClick}><h1>Latest Reviews</h1></button>
+          </div>
+          <div className="header-item">
+            <button className="button" onClick={this.handleSearchClick}><h1>Find</h1></button>
+          </div>
         </div>
-        <div className="search">
-        <h1>Search</h1>
-
-        <Search />
+        <div className="container">
+          {div}
         </div>
       </div>
-
     );
   }
 }
